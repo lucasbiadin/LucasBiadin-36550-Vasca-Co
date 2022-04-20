eab4a87588bdb5cdca6309c335a7c04ed03ed4ad
lucasbiadin <lucas.biadin@gmail.com>
2022-04-20 19:47:44 -0300

    pre-entrega 2

---
+++ carritoIndex.js
@@ -0,0 +1,54 @@
+import { productos } from "./stock.js";
+import { actualizarCarrito } from "./actualizarCarrito.js";
+
+
+let carritoDeCompras = [];
+
+export const carritoIndex = (productoId) => {
+  const contenedorCarrito = document.getElementById('carrito-contenedor');
+  let productoRepetido = carritoDeCompras.find(producto => producto.id == productoId);
+  // const guardarLocal=(clave, valor)=>{
+  //   localStorage.setItem(clave,valor)
+  // }
+  // for (const producto of productos){
+  //   guardarLocal(producto.id, JSON.stringify(producto));
+  // }
+
+  const renderProductosCarrito = () => {
+    let producto = productos.find(producto => producto.id == productoId);
+    carritoDeCompras.push(producto);
+
+    producto.cantidad = 1;
+    let div = document.createElement('div');
+    div.classList.add('productoEnCarrito');
+    div.innerHTML =
+      ` <p>${producto.nombre}</p>
+        <p> Precio: $${producto.precio}</p>
+        <p id="cantidad${producto.id}"> Cantidad: ${producto.cantidad}</p>
+        <button id="eliminar${producto.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`;
+    contenedorCarrito.appendChild(div);
+
+    actualizarCarrito(carritoDeCompras);
+  }
+
+  const contarProductosRepetidos = (prodRepetido) => {
+    if (prodRepetido) {
+      prodRepetido.cantidad++
+      document.getElementById(`cantidad${prodRepetido.id}`).innerHTML = `<p id=cantidad${prodRepetido.id}>Cantidad:${prodRepetido.cantidad}</p>`;
+      actualizarCarrito(carritoDeCompras);
+    } else {
+      renderProductosCarrito(productoId);
+    }
+  }
+  const eliminarProductoCarrito = (productoId) => {
+    let botonEliminar = document.getElementById(`eliminar${productoId}`);
+
+    botonEliminar.addEventListener('click', () => {
+      botonEliminar.parentElement.remove();
+      carritoDeCompras = carritoDeCompras.filter(el => el.id != productoId);
+      actualizarCarrito(carritoDeCompras)
+    })
+  }
+  contarProductosRepetidos(productoRepetido);
+  eliminarProductoCarrito(productoId)
+}
---
+++ main.js
@@ -0,0 +1,33 @@
+import { carritoIndex } from "./carritoIndex.js";
+import { productos } from "./stock.js";
+
+
+
+const mostrarProductos = (productos) => {
+  const contenedorProductos = document.getElementById("producto-contenedor");
+
+  productos.forEach(producto => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML +=
+      `<div class="card-image">
+        <img src=${producto.img}>
+        <div class="contenedor-botonYtituloProducto">
+          <span class="nombre-producto">${producto.nombre}</span>
+          <a class="btn-floating waves-effect waves-light boton-agregar " id=boton${producto.id}><i class="boton-agregar material-icons">add_shopping_cart</i></a>
+        </div>
+      </div>
+      <div class="card-content">
+        <p>${producto.desc}</p>
+        <p>$${producto.precio}</p>
+      </div>`;
+    contenedorProductos.appendChild(div);
+
+    const boton = document.getElementById(`boton${producto.id}`);
+    boton.addEventListener('click', () => {
+      carritoIndex(producto.id);
+    })
+  });
+};
+
+mostrarProductos(productos);
